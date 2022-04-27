0fa85f9a69a4494631615b4c8ca40ed7ef2f1301
mlanciau <mlanciau@gmail.com>
2022-04-27 14:30:53 +0200

    Quick and dirty

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
+import sys
 import tweepy
 import keyring
+import argparse
 import psycopg2
 import psycopg2.extras
-from tweepy import Cursor
+
+# parse argument
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', '--tweepy_Bearer_Token',
+                    help='Tweepy Bearer Token', required=True)
+args = parser.parse_args()
 
 # gather credentials
-tweepy_Bearer_Token = keyring.get_password("system", "tweepy_Bearer_Token")
+tweepy_Bearer_Token = args.tweepy_Bearer_Token
 dataGovOps_database_ip = '127.0.0.1'
 dataGovOps_database_name = 'mlanciau'
 dataGovOps_database_username = 'mlanciau'
-dataGovOps_database_password = keyring.get_password("system", "dataGovOps_database_password")
+dataGovOps_database_password = keyring.get_password(
+    "system", "dataGovOps_database_password")
 
 # grab last id inserted into the DB
-conn = psycopg2.connect(f"dbname={dataGovOps_database_name} user={dataGovOps_database_username} password={dataGovOps_database_password}")
+conn = psycopg2.connect(
+    f"dbname={dataGovOps_database_name} user={dataGovOps_database_username} password={dataGovOps_database_password}")
 cur = conn.cursor()
 cur.execute("""
 CREATE TABLE IF NOT EXISTS t_twitter_postgresql(
@@ -31,23 +40,34 @@ data = cur.fetchone()
 since_id = data[0]
 print(f"since_id={since_id}")
 
+# return_code
+return_code = 0
+
 # query twitter API and insert data into DB
 query = """
 INSERT INTO t_twitter_postgresql(c_id, c_text) VALUES(%s, %s)
 ;
 """
-client = tweepy.Client(tweepy_Bearer_Token)
-tweets = client.search_recent_tweets('postgresql', max_results=100, since_id=since_id)
-values = []
-if tweets.data is not None and len(tweets.data) > 0:
-    for tweet in tweets.data:
-        values.append((tweet.id, tweet.text, ))
-    print(f"{len(values)} values")
-    psycopg2.extras.execute_batch(cur, query, values)
+if tweepy_Bearer_Token is None:
+    print('No bearer token')
+    return_code = 1
 else:
-    print('No Data')
+    # print(tweepy_Bearer_Token)
+    client = tweepy.Client(tweepy_Bearer_Token)
+    tweets = client.search_recent_tweets(
+        'postgresql', max_results=100, since_id=since_id)
+    values = []
+    if tweets.data is not None and len(tweets.data) > 0:
+        for tweet in tweets.data:
+            values.append((tweet.id, tweet.text, ))
+        print(f"{len(values)} values")
+        psycopg2.extras.execute_batch(cur, query, values)
+    else:
+        print('No Data')
 
 # close database connection
 conn.commit()
 cur.close()
 conn.close()
+
+sys.exit(return_code)
