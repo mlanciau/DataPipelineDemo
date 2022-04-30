ad05a65c150a951af9cb8d94ce1e654c81e942e4
mlanciau <mlanciau@gmail.com>
2022-04-30 21:31:23 +0200

    Adding virtualenv quickly

--- grab_twitter_postgresql/grab_twitter_postgresql.sh
+++ grab_twitter_postgresql/grab_twitter_postgresql.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 date
-working_dir=/Users/mlanciau
-echo 'création du répertoire de log'
-mkdir -p ${working_dir}/logs
+user_dir=/Users/mlanciau
+virtualenv_dir=/Users/mlanciau/cron/grab_twitter_postgresql
+echo 'création des répertoires'
+mkdir ${user_dir}/logs
 echo 'changement de répertoire'
-cd ${working_dir}/Documents/GitHub/DataPipelineDemo
+cd ${virtualenv_dir}
+echo 'activation de l'\''environnement python'
+source venv/bin/activate
+echo 'installation des dépendances'
+python3 -m pip install -r ${user_dir}/Documents/GitHub/DataPipelineDemo/grab_twitter_postgresql/requirements.txt
 echo 'exécution du script'
-tweepy_Bearer_Token=$(head -n 1 ${working_dir}/.tweepy_Bearer_Token.txt)
-/usr/bin/python3 ${working_dir}/Documents/GitHub/DataPipelineDemo/grab_twitter_postgresql/main.py -t=${tweepy_Bearer_Token} > ${working_dir}/logs/main.out 2>&1
+tweepy_Bearer_Token=$(head -n 1 ${user_dir}/.tweepy_Bearer_Token.txt)
+python3 ${user_dir}/Documents/GitHub/DataPipelineDemo/grab_twitter_postgresql/main.py -t=${tweepy_Bearer_Token} > ${user_dir}/logs/main.out 2>&1
 CR=$?
 echo 'Return code : '$CR
 if [ ! $CR -eq 0 ];
 then
-  cp ${working_dir}/logs/main.out ${working_dir}/Desktop/grab_twitter_postgresql.out
+  cp ${user_dir}/logs/main.out ${user_dir}/Desktop/grab_twitter_postgresql.out
 fi
