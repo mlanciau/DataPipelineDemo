1af715d6fda469d6298bf450de64dcd17451d8b1
mlanciau <mlanciau@gmail.com>
2022-05-04 06:18:54 +0200

    Add small improvement

--- dbt_demo/dbt_demo.sh
+++ dbt_demo/dbt_demo.sh
@@ -6,7 +6,16 @@ echo 'création des répertoires'
 mkdir ${user_dir}/logs
 echo 'changement de répertoire'
 cd ${virtualenv_dir}
-echo 'source freshness'
+echo 'dbt deps'
+/opt/homebrew/bin/dbt deps > ${user_dir}/logs/dbt_deps.out 2>&1
+CR=$?
+echo 'Return code : '$CR
+if [ ! $CR -eq 0 ];
+then
+  cp ${user_dir}/logs/dbt_deps.out ${user_dir}/Desktop/dbt_deps.out
+  exit $CR
+fi
+echo 'dbt source freshness'
 /opt/homebrew/bin/dbt source freshness > ${user_dir}/logs/dbt_source_freshness.out 2>&1
 CR=$?
 echo 'Return code : '$CR
@@ -15,7 +24,7 @@ then
   cp ${user_dir}/logs/dbt_source_freshness.out ${user_dir}/Desktop/dbt_source_freshness.out
   exit $CR
 fi
-echo 'seed'
+echo 'dbt seed'
 /opt/homebrew/bin/dbt seed > ${user_dir}/logs/dbt_seed.out 2>&1
 CR=$?
 echo 'Return code : '$CR
@@ -24,7 +33,7 @@ then
   cp ${user_dir}/logs/dbt_seed.out ${user_dir}/Desktop/dbt_seed.out
   exit $CR
 fi
-echo 'test'
+echo 'dbt test'
 /opt/homebrew/bin/dbt test > ${user_dir}/logs/dbt_test.out 2>&1
 CR=$?
 echo 'Return code : '$CR
@@ -33,7 +42,7 @@ then
   cp ${user_dir}/logs/dbt_test.out ${user_dir}/Desktop/dbt_test.out
   exit $CR
 fi
-echo 'run'
+echo 'dbt run'
 /opt/homebrew/bin/dbt run > ${user_dir}/logs/dbt_run.out 2>&1
 CR=$?
 echo 'Return code : '$CR
@@ -42,7 +51,7 @@ then
   cp ${user_dir}/logs/dbt_run.out ${user_dir}/Desktop/dbt_run.out
   exit $CR
 fi
-echo 'docs generate'
+echo 'dbt docs generate'
 /opt/homebrew/bin/dbt docs generate > ${user_dir}/logs/dbt_docs_generate.out 2>&1
 CR=$?
 echo 'Return code : '$CR
