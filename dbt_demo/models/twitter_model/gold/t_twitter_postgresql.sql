5f9dd5bf7390233aecabb6c56ba82527e44d1919
mlanciau <mlanciau@gmail.com>
2022-05-26 05:25:07 +0200

    Add incremental feature to DBT

--- dbt_demo/models/twitter_model/gold/t_twitter_postgresql.sql
+++ dbt_demo/models/twitter_model/gold/t_twitter_postgresql.sql
@@ -1,2 +1,13 @@
+{{
+  config(
+    materialized = 'incremental',
+    unique_key = 'c_id'
+  )
+}}
+
 SELECT *
 FROM {{ ref('v_twitter_postgresql') }}
+{% if  is_incremental() %}
+WHERE c_id > (SELECT MAX(c_id) FROM {{ this }})
+{% endif %}
+ 
